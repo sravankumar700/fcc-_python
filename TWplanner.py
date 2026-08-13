aab764b6889f27f63b8c4a7f17c08182e1fe8ee3
Sravankumar700 <chintux66@gmail.com>
2026-08-13 21:56:45 +0530

    Build a Travel Weather Planner

---
+++ TWplanner.py
@@ -0,0 +1,26 @@
+distance_mi = 0
+is_raining = False
+has_bike = False
+has_car = True
+has_ride_share_app = True
+
+if not distance_mi:
+    print(False)
+
+elif distance_mi <= 1:
+    if not is_raining:
+        print(True)
+    else:
+        print(False)
+
+elif distance_mi > 1 and distance_mi <= 6:
+    if has_bike and not is_raining:
+        print(True)
+    else:
+        print(False)
+
+else:
+    if has_car or has_ride_share_app:
+        print(True)
+    else:
+        print(False)
